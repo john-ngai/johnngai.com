79e2a063a9eeaf2615f7d049082c7400d3bffa1c
mrjohnming <john.ngai@hotmail.com>
2022-04-28 18:04:55 -0400

    Update .text-zone

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -17,14 +17,16 @@ export default function About() {
         <h1>About Me</h1>
         <p>
           I am a very ambitious full stack developer looking for a role at
-          an established and innovative company with opportunities to work with the latest technologies on challenging and diverse projects.
+          an established and innovative company with opportunities to work with
+          the latest technologies on challenging and diverse projects.
         </p>
         <p>
           I'm quietly confident, naturally curious, and patiently working on
           mastering my craft, one step at a time.
         </p>
         <p>
-          If I were to define myself in one sentence, that would be a extroverted, bookworm, and tech-obsessed!!!
+          If I were to define myself in one sentence, that would be a extroverted,
+          mentoring, bookworm, and tech-obsessed!!!
         </p>
       </div>
     </div>
