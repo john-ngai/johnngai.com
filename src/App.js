3ec92563c2bcda119bd64b5d1ed9deb2ea5cc708
mrjohnming <john.ngai@hotmail.com>
2022-04-28 14:43:28 -0400

    Create route - /about

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
+import About from './components/About';
 import './App.scss';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path='about' element={<About />} />
       </Route>
     </Routes>
   );
