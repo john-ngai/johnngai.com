2fa0e883331869537e11aed31121733c60357dea
mrjohnming <john.ngai@hotmail.com>
2022-04-28 15:17:54 -0400

    Create component - About

---
+++ src/components/About/index.js
@@ -0,0 +1,30 @@
+import { faJs, faJsSquare, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './index.scss';
+
+export default function About() {
+  return (
+    <div className='container about-page'>
+      <div className='stacks'>
+        <FontAwesomeIcon className='js-icon' icon={faJsSquare} />
+        <FontAwesomeIcon className='react-icon' icon={faReact} />
+        <FontAwesomeIcon className='nodeJs-icon' icon={faNodeJs} />
+      </div>
+      
+      <div className='text-zone'>
+        <h1>About Me</h1>
+        <p>
+          I am a very ambitious full stack developer looking for a role at
+          an established and innovative company with opportunities to work with the latest technologies on challenging and diverse projects.
+        </p>
+        <p>
+          I'm quietly confident, naturally curious, and patiently working on
+          mastering my craft, one step at a time.
+        </p>
+        <p>
+          If I were to define myself in one sentence, that would be a extroverted, bookworm, and tech-obsessed!!!
+        </p>
+      </div>
+    </div>
+  );
+}
