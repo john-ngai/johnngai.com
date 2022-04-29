80703cf6cfee1bff4e5dfa3185b1cb4a06c25d56
mrjohnming <john.ngai@hotmail.com>
2022-04-29 11:03:00 -0400

    Create componet - Contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,7 +4,16 @@ export default function Contact() {
   return (
     <div className='container contact-page'>
       <div className='text-zone'>
-        Contact Component
+        <h1>Contact Me</h1>
+
+        <p>
+          I am interested in full-time opportunities at an
+          established and innovative company. However, if you
+          have other requests or questions, I am always happy to
+          consider them.
+        </p>
+
+        <p className='email'>john.ngai@hotmail.com</p>
       </div>
     </div>
   );
