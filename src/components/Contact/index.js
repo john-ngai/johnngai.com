9b4d713e338495b47c84909c89ee6ea28ece1f77
mrjohnming <john.ngai@hotmail.com>
2022-04-28 20:54:28 -0400

    Initial component setup - Contact

---
+++ src/components/Contact/index.js
@@ -0,0 +1,11 @@
+import './index.scss';
+
+export default function Contact() {
+  return (
+    <div className='container contact-page'>
+      <div className='text-zone'>
+        Contact Component
+      </div>
+    </div>
+  );
+}
