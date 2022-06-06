8811a52a51fa8228fee49e745bd2800685a13f89
mrjohnming <john.ngai@hotmail.com>
2022-06-06 16:19:46 -0400

    Create component - Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
