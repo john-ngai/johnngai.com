d2a4825d7ce755fef7e9d27c88644dc09b86a9af
mrjohnming <john.ngai@hotmail.com>
2022-06-08 17:21:01 -0400

    Import component - Navbar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,19 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../Header'
+import Navbar from '../Navbar';
 import Footer from '../Footer';
 import './index.scss';
 
-export default function Layout() {
+export default function Layout(props) {
+  const { toggleDisplay, displayMenu} = props;
+  
   return (
     <div className='app'>
-      <Header />
+      <Header toggleDisplay={toggleDisplay} />
+      <Navbar
+        toggleDisplay={toggleDisplay}
+        displayMenu={displayMenu}
+      />
       
       <div className="page">
         <Outlet />
