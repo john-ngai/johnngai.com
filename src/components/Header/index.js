1383a0fd99e3c5edc3573bc557e807fc46a358e7
mrjohnming <john.ngai@hotmail.com>
2022-06-08 17:20:22 -0400

    Remove unused import

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import './index.scss';
