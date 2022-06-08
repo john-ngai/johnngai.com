9d5b575509aa3854e96753ef707f08fd375bb727
mrjohnming <john.ngai@hotmail.com>
2022-06-08 14:27:26 -0400

    Add feature - show/hide menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import './index.scss';
+import classNames from 'classnames';
 
 export default function Header() {
+  const [display, setDisplay] = useState(false);
+  
+  const menuClass = classNames('menu', {
+    show: display, hide: !display,
+  });
+
+  const toggleDisplay = () => setDisplay(prev => !prev);
+
   const menuIcon = (
   <FontAwesomeIcon className='menu-icon'
     icon={faBars}
+    onClick={toggleDisplay}
   />
   );
   
@@ -13,6 +24,14 @@ export default function Header() {
     <header>
       <span className='logo'>John Ngai</span>
       {menuIcon}
+      <nav className={menuClass}>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
     </header>
   );
 }
