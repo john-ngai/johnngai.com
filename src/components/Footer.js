181b0b846abfb163cf55d59e7a9183419b272e03
mrjohnming <john.ngai@hotmail.com>
2022-06-08 13:39:29 -0400

    Add footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+
 export default function Footer() {
+  const linkedInIcon = (
+    <FontAwesomeIcon className='linkedin-icon'
+      icon={faLinkedin}
+    />
+  );
+  const githubIcon = (
+    <FontAwesomeIcon className='github-icon'
+      icon={faGithub}
+    />
+  );
+
   return (
-    <footer>Footer Component</footer>
+    <footer>
+      <span className='social-icons'>
+        {linkedInIcon}
+        {githubIcon}
+      </span>
+      <span className="copyright">&copy; 2022 John Ngai</span>
+    </footer>
   );
 }
