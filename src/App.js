b35f78b505aaf1fa360d0449ab4e9308b2f84e0e
mrjohnming <john.ngai@hotmail.com>
2022-06-08 12:37:04 -0400

    Import component - Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
 import './style.scss';
 
 export default function App() {
   return (
     <Routes>
-      <Route path='/' element={''}>
+      <Route path='/' element={<Layout />}>
         <Route index element={''} />
         <Route path='about' element={''} />
         <Route path='projects' element={''} />
