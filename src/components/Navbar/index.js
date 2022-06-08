8f38417fb7c220397f40b0cb7475b3e51f047779
mrjohnming <john.ngai@hotmail.com>
2022-06-08 18:44:23 -0400

    Change styling target from li:first-child to .first-list-item

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,21 +1,33 @@
 import classNames from "classnames";
+import { Link } from "react-router-dom";
 import './index.scss';
 
 export default function Navbar(props) {
   const { toggleDisplay, displayMenu } = props;
-  
+
   const menuClass = classNames('menu', {
     show: displayMenu, hide: !displayMenu,
   });
-  
+
   return (
     <nav className={menuClass}>
       <ul>
-          <li onClick={toggleDisplay}>Home</li>
+        <Link to='/'>
+          <li className="first-list-item" onClick={toggleDisplay}>Home</li>
+        </Link>
+
+        <Link to='/'>
           <li onClick={toggleDisplay}>About</li>
+        </Link>
+        
+        <Link to='projects'>
           <li onClick={toggleDisplay}>Projects</li>
+        </Link>
+        
+        <Link to='/'>
           <li onClick={toggleDisplay}>Contact</li>
-        </ul>
+        </Link>
+      </ul>
     </nav>
   );
 }
