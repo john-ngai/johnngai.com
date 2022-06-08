5c299470a6b8d8943e79e5451918f8ab2d0a2713
mrjohnming <john.ngai@hotmail.com>
2022-06-08 13:21:39 -0400

    Import FontAwesomeIcon - faBars

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
 export default function Header() {
+  const menuIcon = (
+  <FontAwesomeIcon className='menu-icon'
+    icon={faBars}
+  />
+  );
+  
   return (
-    <header>Header Component</header>
+    <header>
+      <span className='logo'>John Ngai</span>
+      {menuIcon}
+    </header>
   );
 }
