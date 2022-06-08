6369f933644d2b9ba994c3f8af26ac76721098a6
mrjohnming <john.ngai@hotmail.com>
2022-06-08 17:22:31 -0400

    Move feature implementation into component - show/hide menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+// Packages
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+// Components
 import Layout from './components/Layout';
 import Home from './components/Home';
+// Stylesheet
 import './style.scss';
 
 export default function App() {
+  const [displayMenu, setDisplayMenu] = useState(false);
+  const toggleDisplay = () => setDisplayMenu(prev => !prev);
+  
   return (
     <Routes>
-      <Route path='/' element={<Layout />}>
+        <Route path='/'
+          element={
+            <Layout
+              toggleDisplay={toggleDisplay}
+              displayMenu={displayMenu}
+            />
+          }
+        >
         <Route index element={<Home />} />
         <Route path='about' element={''} />
         <Route path='projects' element={''} />
