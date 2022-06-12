de4fc0dbeb9c2dd6807024847388fb6115ec8810
mrjohnming <john.ngai@hotmail.com>
2022-06-12 17:48:58 -0400

    Import classnames for conditional rendering

--- src/components/Projects/ProjectItem/index.js
+++ src/components/Projects/ProjectItem/index.js
@@ -1,13 +1,20 @@
+import classNames from 'classnames';
 import './index.scss';
 
 export default function FeaturedProjects(props) {
   const {
-    name, description, languages,
-    frameworks, website, image,
+    name, description,
+    languages, frameworks,
+    website, image,
+    featured,
   } = props;
 
+  const projectClass = classNames('project-item', {
+    featured: featured, other: !featured,
+  });
+
   return (
-    <div className='project-item'>
+    <div className={projectClass}>
       <a href={website} target='_blank' rel='noreferrer'>
         {image && <img src={image} />}
         <h3>{name}</h3>
