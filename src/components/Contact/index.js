88e1a6ac721b4e9d33feccd48770da6109104f75
mrjohnming <john.ngai@hotmail.com>
2022-06-12 20:30:06 -0400

    Add Contact component content & styling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,6 +4,8 @@ export default function Contact() {
   return (
     <section className='container contact'>
       <h2>Contact</h2>
+      <p>For all inquiries, please email me at:</p>
+      <a href='mailto:john.ngai@hotmail.com'><strong>john.ngai@hotmail.com</strong></a>
     </section>
   );
 }
